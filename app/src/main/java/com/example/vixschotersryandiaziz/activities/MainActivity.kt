1036dd0a47b0eb8c1ae791c8ddd3e8b32f4c43d3
Ryandi Aziz <ryandiaziz.edu@gmail.com>
2023-01-27 19:20:13 +0700

    Menambahkan news fragment

---
+++ app/src/main/java/com/example/vixschotersryandiaziz/activities/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.vixschotersryandiaziz.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.vixschotersryandiaziz.R
+import com.example.vixschotersryandiaziz.fragment.*
+import com.ismaeldivita.chipnavigation.ChipNavigationBar
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNavigation.setItemSelected(R.id.menuHeadline, true)
+
+        setupFragment(FragmentHeadline())
+
+        //open fragment
+        bottomNavigation.setOnItemSelectedListener(object : ChipNavigationBar.OnItemSelectedListener {
+            override fun onItemSelected(id: Int) {
+                when (id) {
+                    R.id.menuHeadline -> setupFragment(FragmentHeadline())
+                    R.id.menuSports -> setupFragment(FragmentSports())
+                    R.id.menuTechnology -> setupFragment(FragmentTechnology())
+                    R.id.menuBusiness -> setupFragment(FragmentBusiness())
+                    R.id.menuHealth -> setupFragment(FragmentHealth())
+                    R.id.menuEntertaiment -> setupFragment(FragmentEntertaiment())
+                    R.id.menuSearch -> setupFragment(FragmentSearch())
+                }
+            }
+        })
+    }
+
+    private fun setupFragment(fragment : Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentLayout, fragment)
+        transaction.commit()
+    }
+
+}
---
+++ app/src/main/java/com/example/vixschotersryandiaziz/fragment/FragmentBusiness.kt
@@ -0,0 +1,83 @@
+package com.example.vixschotersryandiaziz.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.vixschotersryandiaziz.R
+import com.example.vixschotersryandiaziz.adapter.NewsAdapter
+import com.example.vixschotersryandiaziz.model.ModelArticle
+import com.example.vixschotersryandiaziz.model.ModelNews
+import com.example.vixschotersryandiaziz.networking.ApiEndpoint.getApiClient
+import com.example.vixschotersryandiaziz.networking.ApiInterface
+import com.example.vixschotersryandiaziz.util.Utils.getCountry
+import kotlinx.android.synthetic.main.fragement_news.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.*
+
+class FragmentBusiness : Fragment() {
+
+    companion object {
+        const val API_KEY = "API KEY ada di Video YT"
+    }
+
+    var strCategory = "business"
+    var strCountry: String? = null
+    var modelArticle: MutableList<ModelArticle> = ArrayList()
+    var newsAdapter: NewsAdapter? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragement_news, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tvTitle.setText("Berita Bisnis")
+
+        rvListNews.setLayoutManager(LinearLayoutManager(context))
+        rvListNews.setHasFixedSize(true)
+        rvListNews.showShimmerAdapter()
+
+        //reload news
+        imageRefresh.setOnClickListener {
+            rvListNews.showShimmerAdapter()
+            getListNews()
+        }
+
+        //get news
+        getListNews()
+    }
+
+    //set api
+    private fun getListNews() {
+
+        //get country/
+        strCountry = getCountry()
+
+        //set api
+        val apiInterface = getApiClient().create(ApiInterface::class.java)
+        val call = apiInterface.getBusiness(strCountry, strCategory, API_KEY)
+        call.enqueue(object : Callback<ModelNews> {
+            override fun onResponse(call: Call<ModelNews>, response: Response<ModelNews>) {
+                if (response.isSuccessful && response.body() != null) {
+                    modelArticle = response.body()?.modelArticle as MutableList<ModelArticle>
+                    newsAdapter = NewsAdapter(modelArticle, context!!)
+                    rvListNews.adapter = newsAdapter
+                    newsAdapter?.notifyDataSetChanged()
+                    rvListNews.hideShimmerAdapter()
+                }
+            }
+
+            override fun onFailure(call: Call<ModelNews>, t: Throwable) {
+                Toast.makeText(context, "Oops, jaringan kamu bermasalah.", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+}
